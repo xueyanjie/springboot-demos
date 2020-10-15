053beade694ec6e44dd61e1a207abcacf826b533
yanjie12 <yanjie12@bj-m-219970a.local>
2020-10-15 11:09:09 +0800

    Add thymeleaf demo

---
+++ thymeleaf/src/main/java/com/demo/thymeleafdemo/controllers/DemoController.java
@@ -0,0 +1,16 @@
+package com.demo.thymeleafdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello(ModelMap map) {
+        map.addAttribute("name", "野猪先生");
+        return "demo";
+    }
+
+}
